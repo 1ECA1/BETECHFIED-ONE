e3bd79e8a9893f3995c07a70422835eeb054d6bc
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2026-01-17 18:37:13 +0100

    Initial commit

---
+++ intro.js
@@ -0,0 +1,5 @@
+const name = "Alexone";
+const age = 23;
+const proLaguage = "JavaScript";
+
+console.log("My name is" + name + " " + "I am " + age +" "  + "years old and  love " + proLaguage + " programming language" )
